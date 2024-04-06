a803a1d57b5ac71c7a7f92c67f68787ef209114f
HoangVuong1910 <huyhoang191072@gmail.com>
2024-04-06 17:24:26 +0700

    Initial folder structure and test Routes of Login with loginController, middleware loginValidator

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import express from 'express'
+import usersRouter from './routes/users.routes'
+const app = express()
+const port = 3056
+app.use(express.json())
+app.use('/v1/api/users', usersRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
