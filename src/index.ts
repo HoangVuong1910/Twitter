6e77b0054dff7f3384af4643d7fc77a52aee4236
HoangVuong1910 <huyhoang191072@gmail.com>
2024-04-06 18:05:07 +0700

    Connect to Mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 const port = 3056
 app.use(express.json())
 app.use('/v1/api/users', usersRouter)
-
+databaseService.connect()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
