6a5bd08b2b38b3f05839de30a655a214f11a2eaf
HoangVuong1910 <huyhoang191072@gmail.com>
2024-07-02 23:57:05 +0700

    refactor: upload multiple images with AWS S3 Bucket Service

--- src/services/medias.services.ts
+++ src/services/medias.services.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import fsPromise from 'fs/promises'
 import { Request } from 'express'
 import path from 'path'
 import sharp from 'sharp'
@@ -8,23 +9,42 @@ import { isProduction } from '~/constants/congfig'
 import { config } from 'dotenv'
 import { MediaType } from '~/constants/enums'
 import { Media } from '~/models/Other'
+import { uploadFileToS3 } from '~/utils/s3'
+// import mime from 'mime'
+
+import { CompleteMultipartUploadCommandOutput } from '@aws-sdk/client-s3'
+
 config()
 
 class MediasService {
   async uploadImage(req: Request) {
     const files = await handleUploadImage(req)
+    const mime = (await import('mime')).default
+
     const result: Media[] = await Promise.all(
       files.map(async (file) => {
         const newName = getNameFromFullname(file.newFilename)
-        const newPath = path.resolve(UPLOAD_IMAGE_DIR, `${newName}.jpg`)
+        const newFullFileName = `${newName}.jpg`
+        const newPath = path.resolve(UPLOAD_IMAGE_DIR, newFullFileName)
         await sharp(file.filepath).jpeg().toFile(newPath) // Xử lý ảnh chuyển sang jpeg và lưu vào folder upload bằng method toFile()
+        //upload file s3
+        const s3Result = await uploadFileToS3({
+          filename: newFullFileName,
+          filepath: newPath,
+          contentType: mime.getType(newPath) as string
+        })
         // fs.unlinkSync(file.filepath) // xóa ảnh trong file tạm sau khi đã xử lý ảnh ( chưa handle được Error deleting file: EPERM: operation not permitted, unlink)
+        // await Promise.all([fsPromise.unlink(file.filepath), fsPromise.unlink(newPath)])
         return {
-          url: isProduction
-            ? `${process.env.HOST}/static/image/${newName}.jpg`
-            : `http://localhost:${process.env.PORT}/v1/api/static/image/${newName}.jpg`,
+          url: (s3Result as CompleteMultipartUploadCommandOutput).Location as string,
           type: MediaType.Image
         }
+        // return {
+        //   url: isProduction
+        //     ? `${process.env.HOST}/static/image/${newFullFileName}`
+        //     : `http://localhost:${process.env.PORT}/v1/api/static/image/${newFullFileName}`,
+        //   type: MediaType.Image
+        // }
       })
     )
     return result
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -13,28 +13,46 @@ const s3 = new S3({
   }
 })
 // s3.listBuckets({}).then((data) => console.log(data))
-const file = fs.readFileSync(path.resolve('uploads/images/403289fc0751f828d07dfe400.jpg'))
-const parallelUploads3 = new Upload({
-  client: s3,
-  params: { Bucket: 'twitter-clone-ap-southeast-1-1910', Key: 'anh1.jpg', Body: file, ContentType: 'image/jpeg' },
 
-  // optional tags
-  tags: [
-    /*...*/
-  ],
-  // additional optional fields show default values below:
-  // (optional) concurrency configuration
-  queueSize: 4,
-  // (optional) size of each part, in bytes, at least 5MB
-  partSize: 1024 * 1024 * 5,
-  // (optional) when true, do not automatically call AbortMultipartUpload when
-  // a multipart upload fails to complete. You should then manually handle
-  // the leftover parts.
-  leavePartsOnError: false
-})
+// const file = fs.readFileSync(path.resolve('uploads/images/403289fc0751f828d07dfe400.jpg'))
 
-parallelUploads3.on('httpUploadProgress', (progress) => {
-  console.log(progress)
-})
+export const uploadFileToS3 = ({
+  filename,
+  filepath,
+  contentType
+}: {
+  filename: string
+  filepath: string
+  contentType: string
+}) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      Bucket: 'twitter-clone-ap-southeast-1-1910',
+      Key: filename,
+      Body: fs.readFileSync(filepath),
+      ContentType: contentType
+    },
 
-parallelUploads3.done().then((res) => console.log(res))
+    // optional tags
+    tags: [
+      /*...*/
+    ],
+    // additional optional fields show default values below:
+    // (optional) concurrency configuration
+    queueSize: 4,
+    // (optional) size of each part, in bytes, at least 5MB
+    partSize: 1024 * 1024 * 5,
+    // (optional) when true, do not automatically call AbortMultipartUpload when
+    // a multipart upload fails to complete. You should then manually handle
+    // the leftover parts.
+    leavePartsOnError: false
+  })
+  return parallelUploads3.done()
+}
+
+// parallelUploads3.on('httpUploadProgress', (progress) => {
+//   console.log(progress)
+// })
+
+// parallelUploads3.done().then((res) => console.log(res))
